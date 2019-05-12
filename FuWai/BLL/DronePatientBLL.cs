a2f37ccfdbfaadbb52eb9737305c0aec93e11d9e
ZDZ <ZDZ@DESKTOP-1C1SU0J>
2019-05-12 12:24:28 +0800

    创建DronePatientBLL与DronePatientDAO

---
+++ FuWai/BLL/DronePatientBLL.cs
@@ -0,0 +1,24 @@
+﻿using FuWai.DAO;
+using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.BLL
+{
+    public class DronePatientBLL
+    {
+
+        DronePatientDAO dao = new DronePatientDAO();
+        public string selectVDronePatient()
+        {
+            DataTable dt = dao.selectVDronePatient();
+            string json = string.Empty;
+            json = JsonHelper.ToJson(dt);         
+            return json;
+        }
+
+    }
+}
