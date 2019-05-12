f3cf4817047703fbb161e37cdfe61151d8800ed6
ZDZ <ZDZ@DESKTOP-1C1SU0J>
2019-05-12 12:57:00 +0800

    添加通过无人机编号查询视图V_DronePatientd的方法

--- FuWai/BLL/DronePatientBLL.cs
+++ FuWai/BLL/DronePatientBLL.cs
@@ -12,12 +12,28 @@ namespace FuWai.BLL
     {
 
         DronePatientDAO dao = new DronePatientDAO();
+
+        /// <summary>
+        /// 查询视图V_DronePatient所有信息
+        /// </summary>
+        /// <returns></returns>
         public string selectVDronePatient()
         {
             DataTable dt = dao.selectVDronePatient();
             string json = string.Empty;
             json = JsonHelper.ToJson(dt);         
             return json;
+        }
+
+        /// <summary>
+        /// 通过无人机编号（droneid）查询视图V_DronePatient所有信息
+        /// </summary>
+        /// <param name="droneid">无人机编号</param>
+        /// <returns></returns>
+        public string selectVDronePatientBydroneid(string droneid)
+        {
+            DataTable dt = dao.selectVDronePatientByDroneid(droneid);
+            return JsonHelper.ToJson(dt);
         }
 
     }
