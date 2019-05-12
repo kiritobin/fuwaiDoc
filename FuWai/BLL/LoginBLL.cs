23100df8e55ec71ed5817bfa0561b0752bcd03c7
Gladiator <2450175030@qq.com>
2019-05-12 11:21:41 +0800

    login

---
+++ FuWai/BLL/LoginBLL.cs
@@ -0,0 +1,31 @@
+﻿using FuWai.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.BLL
+{
+    public class LoginBLL
+    {
+        LoginDAO login = new LoginDAO();
+            /**
+             * 登陆判断
+             * @param user
+             * @param pwd
+             * @return
+             */
+        public Boolean userLogin(String user, String pwd)
+        {
+            int row = login.userLogin(user, pwd);
+            if (row > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FuWai/DAO/LoginDAO.cs
@@ -0,0 +1,28 @@
+﻿using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+
+namespace FuWai.DAO
+{
+    public class LoginDAO
+    {
+        SQLHelper db = new SQLHelper();
+        /*
+         * 登录查询
+         */
+        public int userLogin(String user, String pwd)
+        {
+            int row = 0;
+            String sql = "select count(id) from T_Admin where Adminid=@Adminid and pwd=@pwd";
+            string[] param = {"@Adminid", "@pwd" };
+            object[] value = { user , pwd };
+            row = db.ExecuteNoneQuery(sql, param, value);
+           
+            return row;
+        }
+    }
+}
