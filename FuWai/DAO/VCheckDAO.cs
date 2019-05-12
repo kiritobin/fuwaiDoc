6b8fc93cf02f5998379844bacab68c615ded946c
mz <mz@DESKTOP-LUO88QN>
2019-05-12 12:29:16 +0800

    查询检查记录

---
+++ FuWai/DAO/VCheckDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using FuWai.DBHelper;
+
+namespace FuWai.DAO
+{
+    public class VCheckDAO
+    {
+        SQLHelper db = new SQLHelper();
+        public DataTable selectVCheck()
+        {
+            string sql = "select * from V_Check";
+            DataTable dt = db.FillDataSet(sql, null, null).Tables[0];
+            return dt;
+        }
+    }
+}
