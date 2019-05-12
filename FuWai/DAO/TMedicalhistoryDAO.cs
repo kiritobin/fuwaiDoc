5d1fcc55dc930f2482bf04f0e386b167c990d209
ZDZ <ZDZ@DESKTOP-1C1SU0J>
2019-05-12 16:38:17 +0800

    添加病史（Medicalhistory）的DAO,BLL,action

---
+++ FuWai/DAO/TMedicalhistoryDAO.cs
@@ -0,0 +1,85 @@
+﻿using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.DAO
+{
+    public class TMedicalhistoryDAO
+    {
+
+        SQLHelper db = new SQLHelper();
+
+        /// <summary>
+        /// 病史查询
+        /// </summary>
+        /// <returns>返回所有数据集合DataTable</returns>
+        public DataTable SelectMedicalhistory()
+        {
+            string sql = "select * from T_Medicalhistory";
+            return db.FillDataSet(sql, null, null).Tables[0];
+        }
+
+        /// <summary>
+        /// 通过病史编号（id）查询
+        /// </summary>
+        /// <param name="droneid">无人机编号</param>
+        /// <returns>返回数据集合DataTable</returns>
+        public DataTable SelecttMedicalhistoryByID(string medicalhistoryid)
+        {
+            string sql = "select * from T_Medicalhistory where medicalhistoryid=@medicalhistoryid";
+            string[] param = { "@medicalhistoryid" };
+            object[] value = { medicalhistoryid };
+            return db.FillDataSet(sql, param, value).Tables[0];
+        }
+
+        /// <summary>
+        /// 添加病史信息
+        /// </summary>
+        /// <param name="medicalhistoryreason">病因</param>
+        /// <param name="patientid">病人编号</param>
+        /// <returns></returns>
+        public int insert(string medicalhistoryreason, string patientid)
+        {
+            string sql = "insert into T_Medicalhistory values(@medicalhistoryreason,@patientid)";
+
+            string[] param = { "@medicalhistoryreason", "@patientid"};
+            object[] value = { medicalhistoryreason, patientid };
+
+            return db.ExecuteNoneQuery(sql, param, value);
+        }
+
+        /// <summary>
+        /// 修改病史信息
+        /// </summary>
+        /// <param name="medicalhistoryreason">病因</param>
+        /// <param name="patientid">病人编号</param>
+        /// <returns></returns>
+        public int update(string medicalhistoryreason, string patientid)
+        {
+            string sql = "update T_Medicalhistory set medicalhistoryreason=@medicalhistoryreason ,patientid=@patientid";
+
+            string[] param = { "@medicalhistoryreason", "@patientid"};
+            object[] value = { medicalhistoryreason, patientid };
+
+            return db.ExecuteNoneQuery(sql, param, value);
+        }
+        /// <summary>
+        /// 删除病史信息
+        /// </summary>
+        /// <param name="medicalhistoryid">编号</param>
+        /// <returns></returns>
+        public int delete(string medicalhistoryid)
+        {
+            string sql = "delete from T_Medicalhistory where medicalhistoryid=@medicalhistoryid ";
+
+            string[] param = { "@medicalhistoryid" };
+            object[] value = { medicalhistoryid };
+
+            return db.ExecuteNoneQuery(sql, param, value);
+        }
+
+    }
+}
---
+++ FuWai/action/TMedicalhistory.ashx.cs
@@ -0,0 +1,102 @@
+﻿using FuWai.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.action
+{
+    /// <summary>
+    /// TMedicalhistory 的摘要说明
+    /// </summary>
+    public class TMedicalhistory : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string op = context.Request["op"];
+            if (op == "insert")
+            {
+                insert(context);
+            }
+            else if (op == "update")
+            {
+                update(context);
+            }
+            else if (op == "delete")
+            {
+                delete(context);
+            }
+        }
+
+        TMedicalhistoryBLL medic = new TMedicalhistoryBLL();
+
+        private void loadMedicalhistory(HttpContext context)
+        {
+            String json = medic.getMedicalhistory();
+            context.Response.Write(json);
+            context.Response.End();
+
+        }
+
+        private void insert(HttpContext context)
+        {
+            String medicalhistoryreason = context.Request["medicalhistoryreason"];
+            String patientid = context.Request["patientid"];
+
+            if (medic.insert(medicalhistoryreason, patientid))
+            {
+                context.Response.Write("添加成功");
+                context.Response.End();
+
+            }
+            else
+            {
+                context.Response.Write("添加失败");
+                context.Response.End();
+            }
+        }
+
+        private void update(HttpContext context)
+        {
+            String medicalhistoryreason = context.Request["medicalhistoryreason"];
+            String patientid = context.Request["patientid"];
+
+            if (medic.update(medicalhistoryreason, patientid))
+            {
+                context.Response.Write("修改成功");
+                context.Response.End();
+
+            }
+            else
+            {
+                context.Response.Write("修改失败");
+                context.Response.End();
+            }
+        }
+        private void delete(HttpContext context)
+        {
+            String medicalhistoryid = context.Request["medicalhistoryid"];
+
+            if (medic.delete(medicalhistoryid))
+            {
+                context.Response.Write("删除成功");
+                context.Response.End();
+
+            }
+            else
+            {
+                context.Response.Write("删除失败");
+                context.Response.End();
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
