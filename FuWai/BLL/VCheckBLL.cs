449fa6d02e58e7c1ace4a70d3c63d8d245b097d4
mz <mz@DESKTOP-LUO88QN>
2019-05-12 12:55:36 +0800

    check视图更改和.csproj文件更改

--- FuWai/BLL/VCheckBLL.cs
+++ FuWai/BLL/VCheckBLL.cs
@@ -11,12 +11,26 @@ namespace FuWai.BLL
     public class VCheckBLL
     {
         VCheckDAO dao = new VCheckDAO();
+        /// <summary>
+        /// 查询所有检查记录
+        /// </summary>
+        /// <returns>datatable的表格</returns>
         public string selectVCheck()
         {
             DataTable dt = dao.selectVCheck();
             string jsonString = string.Empty;
             jsonString = JsonHelper.ToJson(dt);
             return jsonString;
+        }
+        /// <summary>
+        /// 根据病人编号获取病人的检查记录
+        /// </summary>
+        /// <param name="patientid">病人编号</param>
+        /// <returns></returns>
+        public string selectByPatientId(string patientid)
+        {
+            DataTable dt = dao.selectByPatientId(patientid);
+            return JsonHelper.ToJson(dt);
         }
     }
 }
--- FuWai/DAO/VCheckDAO.cs
+++ FuWai/DAO/VCheckDAO.cs
@@ -10,10 +10,27 @@ namespace FuWai.DAO
     public class VCheckDAO
     {
         SQLHelper db = new SQLHelper();
+        /// <summary>
+        /// 查询所有检查记录
+        /// </summary>
+        /// <returns>datatable的表格</returns>
         public DataTable selectVCheck()
         {
             string sql = "select * from V_Check";
             DataTable dt = db.FillDataSet(sql, null, null).Tables[0];
+            return dt;
+        }
+        /// <summary>
+        /// 根据病人编号获取病人的检查记录
+        /// </summary>
+        /// <param name="patientid">病人编号</param>
+        /// <returns></returns>
+        public DataTable selectByPatientId(string patientid)
+        {
+            String sql = "select * from V_Check where patientid=@patientid";
+            String[] param = { "@patientid" };
+            object[] value = { patientid };
+            DataTable dt = db.FillDataSet(sql, param, value).Tables[0];
             return dt;
         }
     }
