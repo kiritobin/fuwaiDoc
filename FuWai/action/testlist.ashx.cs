8647dc96bcc772c5ca0f6d613dc59d0b5c1c217c
Admin 王 <Admin @DESKTOP-1BBP2JL>
2019-05-15 15:04:54 +0800

    List 转json

---
+++ FuWai/action/testlist.ashx.cs
@@ -0,0 +1,66 @@
+﻿using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web;
+
+namespace FuWai.action
+{
+    /// <summary>
+    /// testlist 的摘要说明
+    /// </summary>
+    public class testlist : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            test(context);
+        }
+
+        public void test(HttpContext context)
+        {
+            List<testmodel> tmlist = new List<testmodel>();
+            testmodel tm = new testmodel();
+            tm.Patientid = "0001";
+            tm.Level = "1";
+            tm.X = "198.7";
+            tm.Y = "63.7";
+            tmlist.Add(tm);
+
+            testmodel tm1 = new testmodel();
+            tm1.Patientid = "0002";
+            tm1.Level = "1";
+            tm1.X = "19.4";
+            tm1.Y = "63.7";
+            tmlist.Add(tm1);
+
+            String json = JsonSerializer(tmlist);
+            context.Response.Write(json);
+            context.Response.End();
+
+        }
+
+        public static string JsonSerializer<T>(T t)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ser.WriteObject(ms, t);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FuWai/action/testmodel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.action
+{
+    public class testmodel
+    {
+        private string patientid;   //病人id
+        private string level;       //等级
+        private string x;          //坐标
+        private string y;          //坐标
+
+
+        public string Patientid
+        {
+            get
+            {
+                return patientid;
+            }
+
+            set
+            {
+                patientid = value;
+            }
+        }
+
+        public string Level
+        {
+            get
+            {
+                return level;
+            }
+
+            set
+            {
+                level = value;
+            }
+        }
+
+        public string X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public string Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+    }
+}
