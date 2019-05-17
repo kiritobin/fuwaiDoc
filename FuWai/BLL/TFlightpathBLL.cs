7aac41a625995c2e0eaef402ad15bf2328b540d8
Gladiator <2450175030@qq.com>
2019-05-17 12:40:03 +0800

    飞行路径

---
+++ FuWai/BLL/TFlightpathBLL.cs
@@ -0,0 +1,52 @@
+﻿using FuWai.DAO;
+using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.BLL
+{
+    public class TFlightpathBLL
+    {
+        TFlightpathDAO td = new TFlightpathDAO();
+        /// <summary>
+        /// 通过无人机id查询飞行路径
+        /// </summary>
+        /// <param name="droneid">无人机id</param>
+        /// <returns>返回json</returns>
+        public String SelectFlightpathbyDroneid(string droneid)
+        {
+            return JsonHelper.ToJson(td.SelectFlightpathbyDroneid(droneid));
+        }
+
+        /// <summary>
+        /// 通过时间查询飞行路径
+        /// </summary>
+        /// <param name="flighttime">飞行时间</param>
+        /// <returns>返回json</returns>
+        public String SelectFlightpathbytime(string flighttime)
+        {
+            return JsonHelper.ToJson(td.SelectFlightpathbytime(flighttime));
+        }
+
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="droneid">无人机id</param>
+        /// <param name="flighttime">飞行时间</param>
+        /// <param name="lat">经度</param>
+        /// <param name="lng">纬度</param>
+        /// <returns>成功返回true失败返回fasle</returns>
+        public Boolean insert(string droneid, string flighttime, double lat, double lng)
+        {
+            int row = td.insert(droneid, flighttime, lat, lng);
+            if (row > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ FuWai/DAO/TFlightpathDAO.cs
@@ -0,0 +1,59 @@
+﻿using FuWai.DBHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace FuWai.DAO
+{
+    public class TFlightpathDAO
+    {
+        SQLHelper db = new SQLHelper();
+
+        /// <summary>
+        /// 通过无人机id查询飞行路径
+        /// </summary>
+        /// <param name="droneid">无人机id</param>
+        /// <returns>DataTable</returns>
+        public DataTable SelectFlightpathbyDroneid(string droneid)
+        {
+            string sql = "select * from T_Flightpath where droneid=@droneid";
+            string[] param = { "@droneid" };
+            object[] value = { droneid };
+            return db.FillDataSet(sql, param, value).Tables[0];
+        }
+
+        /// <summary>
+        /// 通过时间查询飞行路径
+        /// </summary>
+        /// <param name="flighttime">飞行时间</param>
+        /// <returns>DataTable</returns>
+        public DataTable SelectFlightpathbytime(string flighttime)
+        {
+            string sql = "select * from T_Flightpath where flighttime=@flighttime";
+            string[] param = { "@flighttime" };
+            object[] value = { flighttime };
+            return db.FillDataSet(sql, param, value).Tables[0];
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="droneid">无人机id</param>
+        /// <param name="flighttime">飞行时间</param>
+        /// <param name="lat">经度</param>
+        /// <param name="lng">纬度</param>
+        /// <returns>int</returns>
+        public int insert(string droneid,string flighttime, double lat , double lng)
+        {
+            string sql = "insert into T_Flightpath values(@droneid,@flighttime,@lat,@lng)";
+
+            string[] param = { "@droneid", "@flighttime", "@lat", "lng" };
+            object[] value = { droneid, flighttime, lat, lng };
+
+            return db.ExecuteNoneQuery(sql, param, value);
+        }
+        
+    }
+}
