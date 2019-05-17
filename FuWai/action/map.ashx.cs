52cea6529dc75fa05fd1d6532bb32b3c4e4ff7d7
Gladiator <2450175030@qq.com>
2019-05-17 10:49:35 +0800

    xiug

--- FuWai/action/map.ashx.cs
+++ FuWai/action/map.ashx.cs
@@ -19,26 +19,43 @@ namespace FuWai.action
             {
                 patientAbout(context);
             }
+            else if (op == "") {
+                patientandDroneAbout(context);
+            }
         }
 
         TPatientBLL tpb = new TPatientBLL();
         VMedicalHistoryBLL vmh = new VMedicalHistoryBLL();
-        VPatientTreatmentBLL vpt = new VPatientTreatmentBLL();
-        VPatientCheckBLL vpc = new VPatientCheckBLL();
+        TDiagnoseBLL tdb = new TDiagnoseBLL();
+        TUsedrugBLL tub = new TUsedrugBLL();
+        TOperationBLL tob = new TOperationBLL();
         VGContactBLL vgcb = new VGContactBLL();
         private void patientAbout(HttpContext context)
         {
             String pid = context.Request["patientid"];
 
-            String pinfo = tpb.getinfobydroneid(pid);
+            String pinfo = tpb.getinfobypatientid(pid);
             String phisty = vmh.getinfobypatientid(pid);
-            String pvptt = vpt.SelectTreatmentByPatientID(pid);
-            String pvpc = vpc.selectCheckByPatientId(pid);
+            String pvptt = tdb.SelectDiagnoseBypatientid(pid);
+            String ptub = tub.selectByPatientId(pid);
+            String ptob = tob.selectByPatientId(pid);
             String pvgcb = vgcb.selectVGContactByPatientId(pid);
 
-            context.Response.Write(pinfo + "|-|" + phisty + "|-|" + pvptt+ "|-|" + pvpc + "|-|" + pvgcb);
+            context.Response.Write(pinfo + "|-|" + phisty + "|-|" + pvptt+ "|-|" + ptub + "|-|" + ptob + "|-|" + pvgcb);
             context.Response.End();
         }
+
+        TDroneBLL tdreb = new TDroneBLL();
+        private void patientandDroneAbout(HttpContext context)
+        {
+
+            String pinfo = tpb.getPatient();
+            String dinfo = tdreb.getDroneinfo();
+
+            context.Response.Write(pinfo + "|-|" + dinfo);
+            context.Response.End();
+        }
+
         public bool IsReusable
         {
             get
